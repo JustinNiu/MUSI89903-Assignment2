732fab5af810723755484e835e7597df568f2307
JustinNiu <buptnsy@gmail.com>
2016-02-10 15:23:05 -0500

    vibrato partly implemented

--- src/inc/vibrato.h
+++ src/inc/vibrato.h
@@ -9,5 +9,55 @@
 #ifndef vibrato_h
 #define vibrato_h
 
+#include <stdio.h>
+#include "RingBuffer.h"
+#include "LFO.h"
+#include "ErrorDef.h"
+
+class vibrato{
+    
+    CRingBuffer<float> **fRingBuff;
+    LFO_Niu ppcLFO;
+    
+    
+public:
+    void process(float **ppfInBuff,float **ppfOutbuff, int iNumOfFrames);
+    
+    
+};
+
+
+//class Vibrato{
+//private:
+//    CRingBuffer<float> **ppfRingBuff;
+//    LFO ppcLFO;
+//    
+//    enum VibratoParam {
+//        kModFreq,
+//        kWidth,
+//        kDelay,
+//        kNumParams
+//    };
+//    
+//    float fVibParam[kNumParams];//ModFreq,Width,
+//    float fVibParamRange[kNumParams][3];
+//    float SamplingRate;
+//    int iNumChannels;   //Width in seconds,ModFreq in Hz
+//    int iFramesProcessed;
+//    
+//public:
+//    void process(float **ppfInBuff,float **ppfOutbuff, int iNumOfFrames);
+//    
+//    void setVibratoParam(VibratoParam eVibParam, float fParamVal);
+//    float getVibratoParam(VibratoParam eVibParam) const;
+//    
+//    
+//protected:
+//    Vibrato(float fVParam[3], int UserNumChannels,float UserSamplingRate, int iMaxDelayInSec);
+//    virtual ~Vibrato();
+//};
+
 
 #endif /* vibrato_h */
+
+
