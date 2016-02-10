0e14019d20fbebc56df0bb1e4fd05688b6234a68
JustinNiu <buptnsy@gmail.com>
2016-02-10 12:28:55 -0500

    LFO.h & vibrato.cpp initial submit

---
+++ src/MUSI8903Exec/vibrato.cpp
@@ -0,0 +1,18 @@
+//
+//  vibrato.cpp
+//  MUSI8903
+//
+//  Created by Siyuan Niu on 2/10/16.
+//
+//
+
+#include <stdio.h>
+#include "MUSI8903Config.h"
+#include "vibrato.h"
+#include "ErrorDef.h"
+#include "RingBuffer.h"
+
+
+
+
+
---
+++ src/inc/LFO.h
@@ -0,0 +1,23 @@
+//
+//  LFO.h
+//  MUSI8903
+//
+//  Created by Siyuan Niu on 2/10/16.
+//
+//
+
+#ifndef LFO_h
+#define LFO_h
+
+#define PI 3.14159
+
+class LFO_Niu{
+    float freq;
+    float amp;
+    
+public:
+    
+};
+
+
+#endif /* LFO_h */
