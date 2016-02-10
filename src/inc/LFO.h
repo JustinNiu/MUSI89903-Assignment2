008dbc1a75cb8951d0c914b6825530897159a853
JustinNiu <buptnsy@gmail.com>
2016-02-10 12:58:40 -0500

    LFO.h main implementation

--- src/inc/LFO.h
+++ src/inc/LFO.h
@@ -11,13 +11,34 @@
 
 #define PI 3.14159
 
+//declare the LFO class
+
 class LFO_Niu{
+    
     float freq;
-    float amp;
+//    float amp;
     
 public:
     
+    LFO();
+    ~LFO(){
+        freq = 0;
+    }
+    void setFreq();
+//    void setAmp();
+    
+    void getSine();
 };
 
+
+
+void LFO_Niu::setFreq(float thisFreq){
+    freq = thisFreq;
+//    amp = thisAmp;
+}
+
+void LFO_Niu::getSine(float TimeInSec){
+    return sin(2 * PI *freq * TimeInSec)
+}
 
 #endif /* LFO_h */
