46d700bedaaecb254485ec552067f805b06f47ea
Hua Xiao <shawn.huaxiao@gmail.com>
2016-02-10 19:58:04 -0500

    vibrate declaration and implementation

--- src/inc/vibrato.h
+++ src/inc/vibrato.h
@@ -2,7 +2,7 @@
 //  vibrato.h
 //  MUSI8903
 //
-//  Created by Siyuan Niu on 2/9/16.
+//  Created by Hua on 10/02/2016
 //
 //
 
@@ -14,54 +14,47 @@
 #include "LFO.h"
 #include "ErrorDef.h"
 
-class vibrato{
-    
-    CRingBuffer<float> **fRingDelayLine;
-    LFO_Niu* ppcSine;
-    float freq;
-    int width;
-    int delay;
-    int numVar;
+class CVibrato{
+
+private:
+
+    CRingBuffer<float> **fRingBuff;
+    LFO_Niu* ppcLFO;
+	CVibrato(float maxwidth_secs, int num_channels, long int sample_rate);
+	virtual ~CVibrato();
+
+	float LFOFreq;
+	int width;
+	int entireDelay;
+	int LFOAmp;
+	long int sampleRate;
+	int numChannels;
+	long long int processedSamples;
+
+	Error_t convertTimeToSamples(float paramValue, int& param);
     
     
 public:
-    void process(float **ppfInBuff,float **ppfOutbuff, int iNumOfFrames);
-    
-    
+	//initialisation and reset functions
+	Error_t init(float modFreq, float delayWidthInSecs, float modAmpSecs);
+	Error_t reset();
+
+	static Error_t create(CVibrato*& pCKortIf, long int sample_rate, int num_channels);
+	static Error_t destroy(CVibrato*& pCKortIf);
+
+
+	//set parameter functions
+	//Error_t setDelayWidth(float delay_width_secs);
+	Error_t setModAmp(float modAmpSecs);
+	Error_t setModFreq(float modFreq);
+
+	//get parameter functions
+	Error_t getDelayWidth(int& delayWidth);
+	Error_t getModAmp(int& modAmp);
+	Error_t getModFreq(float& modFreq);
+
+	//process function
+	Error_t process(float **ppfInputBuffer, float **ppfOutputBuffer, int iNumberOfFrames);    
 };
 
-
-//class Vibrato{
-//private:
-//    CRingBuffer<float> **ppfRingBuff;
-//    LFO ppcLFO;
-//    
-//    enum VibratoParam {
-//        kModFreq,
-//        kWidth,
-//        kDelay,
-//        kNumParams
-//    };
-//    
-//    float fVibParam[kNumParams];//ModFreq,Width,
-//    float fVibParamRange[kNumParams][3];
-//    float SamplingRate;
-//    int iNumChannels;   //Width in seconds,ModFreq in Hz
-//    int iFramesProcessed;
-//    
-//public:
-//    void process(float **ppfInBuff,float **ppfOutbuff, int iNumOfFrames);
-//    
-//    void setVibratoParam(VibratoParam eVibParam, float fParamVal);
-//    float getVibratoParam(VibratoParam eVibParam) const;
-//    
-//    
-//protected:
-//    Vibrato(float fVParam[3], int UserNumChannels,float UserSamplingRate, int iMaxDelayInSec);
-//    virtual ~Vibrato();
-//};
-
-
-#endif /* vibrato_h */
-
-
+#endif /* Vibrato_h */
